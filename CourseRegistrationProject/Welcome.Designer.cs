a6f02ab9080d9217757a0ec51e938b553639cae7
oguzk <oguzcankilickaya@gmail.com>
2022-05-13 03:00:00 +0300

    son23

--- CourseRegistrationProject/Welcome.Designer.cs
+++ CourseRegistrationProject/Welcome.Designer.cs
@@ -29,23 +29,33 @@ namespace CourseRegistrationProject
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Welcome));
             this.btnRegisteration = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.btnEnterance = new System.Windows.Forms.Button();
             this.panel4 = new System.Windows.Forms.Panel();
-            this.btnCourseCreation = new System.Windows.Forms.Button();
             this.btnReports = new System.Windows.Forms.Button();
+            this.btnCourseCreation = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.panel2.SuspendLayout();
             this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // btnRegisteration
             // 
-            this.btnRegisteration.Location = new System.Drawing.Point(12, 136);
+            this.btnRegisteration.FlatAppearance.BorderSize = 0;
+            this.btnRegisteration.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRegisteration.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnRegisteration.Location = new System.Drawing.Point(0, 206);
             this.btnRegisteration.Name = "btnRegisteration";
-            this.btnRegisteration.Size = new System.Drawing.Size(175, 51);
+            this.btnRegisteration.Size = new System.Drawing.Size(202, 51);
             this.btnRegisteration.TabIndex = 0;
             this.btnRegisteration.Text = "Registration";
             this.btnRegisteration.UseVisualStyleBackColor = true;
@@ -53,29 +63,33 @@ namespace CourseRegistrationProject
             // 
             // panel2
             // 
-            this.panel2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.panel2.BackColor = System.Drawing.SystemColors.Info;
             this.panel2.Controls.Add(this.label1);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Location = new System.Drawing.Point(202, 0);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1035, 67);
+            this.panel2.Size = new System.Drawing.Size(833, 52);
             this.panel2.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Georgia", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(326, 9);
+            this.label1.BackColor = System.Drawing.SystemColors.Info;
+            this.label1.Font = new System.Drawing.Font("Bahnschrift Condensed", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(229, 2);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(400, 31);
+            this.label1.Size = new System.Drawing.Size(355, 45);
             this.label1.TabIndex = 0;
             this.label1.Text = "Course Registration System";
             // 
             // btnEnterance
             // 
-            this.btnEnterance.Location = new System.Drawing.Point(12, 42);
+            this.btnEnterance.FlatAppearance.BorderSize = 0;
+            this.btnEnterance.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEnterance.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnEnterance.Location = new System.Drawing.Point(0, 149);
             this.btnEnterance.Name = "btnEnterance";
-            this.btnEnterance.Size = new System.Drawing.Size(175, 51);
+            this.btnEnterance.Size = new System.Drawing.Size(202, 51);
             this.btnEnterance.TabIndex = 1;
             this.btnEnterance.Text = "Enterance";
             this.btnEnterance.UseVisualStyleBackColor = true;
@@ -83,45 +97,87 @@ namespace CourseRegistrationProject
             // 
             // panel4
             // 
-            this.panel4.BackColor = System.Drawing.SystemColors.ButtonShadow;
-            this.panel4.Controls.Add(this.btnRegisteration);
-            this.panel4.Controls.Add(this.btnCourseCreation);
+            this.panel4.BackColor = System.Drawing.Color.Peru;
             this.panel4.Controls.Add(this.btnReports);
             this.panel4.Controls.Add(this.btnEnterance);
+            this.panel4.Controls.Add(this.btnCourseCreation);
+            this.panel4.Controls.Add(this.btnRegisteration);
+            this.panel4.Controls.Add(this.pictureBox1);
             this.panel4.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel4.Location = new System.Drawing.Point(0, 67);
+            this.panel4.Location = new System.Drawing.Point(0, 0);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(202, 485);
+            this.panel4.Size = new System.Drawing.Size(202, 552);
             this.panel4.TabIndex = 3;
-            // 
-            // btnCourseCreation
-            // 
-            this.btnCourseCreation.Location = new System.Drawing.Point(12, 227);
-            this.btnCourseCreation.Name = "btnCourseCreation";
-            this.btnCourseCreation.Size = new System.Drawing.Size(175, 51);
-            this.btnCourseCreation.TabIndex = 1;
-            this.btnCourseCreation.Text = "Course Creation";
-            this.btnCourseCreation.UseVisualStyleBackColor = true;
-            this.btnCourseCreation.Click += new System.EventHandler(this.btnCourseCreation_Click);
             // 
             // btnReports
             // 
-            this.btnReports.Location = new System.Drawing.Point(12, 319);
+            this.btnReports.FlatAppearance.BorderSize = 0;
+            this.btnReports.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReports.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnReports.Location = new System.Drawing.Point(0, 320);
             this.btnReports.Name = "btnReports";
-            this.btnReports.Size = new System.Drawing.Size(175, 51);
+            this.btnReports.Size = new System.Drawing.Size(202, 51);
             this.btnReports.TabIndex = 2;
             this.btnReports.Text = "Reports";
             this.btnReports.UseVisualStyleBackColor = true;
             this.btnReports.Click += new System.EventHandler(this.btnReports_Click);
             // 
+            // btnCourseCreation
+            // 
+            this.btnCourseCreation.FlatAppearance.BorderSize = 0;
+            this.btnCourseCreation.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCourseCreation.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnCourseCreation.Location = new System.Drawing.Point(0, 263);
+            this.btnCourseCreation.Name = "btnCourseCreation";
+            this.btnCourseCreation.Size = new System.Drawing.Size(202, 51);
+            this.btnCourseCreation.TabIndex = 1;
+            this.btnCourseCreation.Text = "Course Creation";
+            this.btnCourseCreation.UseVisualStyleBackColor = true;
+            this.btnCourseCreation.Click += new System.EventHandler(this.btnCourseCreation_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = global::CourseRegistrationProject.Properties.Resources.png_transparent_computer_icons_arshad_ayub_graduate_business_school_students_miscellaneous_logo_monochrome;
+            this.pictureBox1.Location = new System.Drawing.Point(42, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(118, 115);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.BackColor = System.Drawing.SystemColors.Info;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.pictureBox2);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(202, 67);
+            this.panel1.Location = new System.Drawing.Point(202, 52);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(833, 485);
+            this.panel1.Size = new System.Drawing.Size(833, 500);
             this.panel1.TabIndex = 4;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(288, 137);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(209, 213);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Info;
+            this.label2.Font = new System.Drawing.Font("Bahnschrift Condensed", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(321, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(141, 45);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Welcome !";
             // 
             // Welcome
             // 
@@ -129,13 +185,17 @@ namespace CourseRegistrationProject
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1035, 552);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.panel4);
             this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel4);
             this.Name = "Welcome";
             this.Text = "Welcome";
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             this.panel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -149,6 +209,9 @@ namespace CourseRegistrationProject
         private System.Windows.Forms.Button btnCourseCreation;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button btnReports;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 

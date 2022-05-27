d0b28a2e80977c765f0159977a1f48e69180542f
oguzk <oguzcankilickaya@gmail.com>
2022-05-27 14:46:00 +0300

    DSDF

--- CourseRegistrationProject/Enterance.cs
+++ CourseRegistrationProject/Enterance.cs
@@ -7,11 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace CourseRegistrationProject
 {
     public partial class Enterance : Form
     {
+        SqlConnection conn;
+        SqlCommand com = new SqlCommand();
         public Enterance()
         {
             InitializeComponent();
@@ -19,6 +23,8 @@ namespace CourseRegistrationProject
             lblID.Visible = false;
             txtPassword.Visible = false;
             lblPassword.Visible = false;
+            String ConnectionString = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+            conn = new SqlConnection(ConnectionString);
         }
         public void loadForm2(UserControl userControl)
         {
@@ -50,14 +56,49 @@ namespace CourseRegistrationProject
 
         private void btnEnter_Click(object sender, EventArgs e)
         {
+            conn.Open();
+            
             if (cbDepartment.Text == "Student")
-                loadForm2(new StudentPage(txtID.Text));
-            else if (cbDepartment.Text == "Instructor")
-                loadForm2(new InstructorPage(txtID.Text));
+            {
+                SqlDataAdapter sda = new SqlDataAdapter("SELECT COUNT(*) FROM STUDENT WHERE STUDENT_ID='" + txtID.Text + "' AND STD_PASSWORD ='" + txtPassword.Text + "'", conn);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                if(dt.Rows[0][0].ToString() == "1") { 
+                    loadForm2(new StudentPage(txtID.Text));
+                }
+                else
+                {
+                    MessageBox.Show("Wrong Username or Password\n\nPlease Try Again");
+                    txtID.Clear();
+                    txtPassword.Clear();
+                    cbDepartment.SelectedIndex = -1;
+                }
+                
+            }
+            else if (cbDepartment.Text == "Instructor") {
+                SqlDataAdapter sda = new SqlDataAdapter("SELECT COUNT(*) FROM INSTRUCTOR WHERE INST_ID='" + txtID.Text + "' AND INST_PASSWORD ='" + txtPassword.Text + "'", conn);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                if (dt.Rows[0][0].ToString() == "1")
+                {
+                    loadForm2(new InstructorPage(txtID.Text));
+                }
+                else
+                {
+                    MessageBox.Show("Wrong Username or Password\n\nPlease Try Again");
+                    txtID.Clear();
+                    txtPassword.Clear();
+                    cbDepartment.SelectedIndex = -1;
+                }
+
+
+            }
+                
             else
             {
                 MessageBox.Show("Please select your Type");
             }
+            conn.Close();
         }
 
        
